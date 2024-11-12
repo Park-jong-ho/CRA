e73bcea3aa754dadf9fd704076d542f80b76c114
jongho <whjk1104@gmail.com>
2024-11-12 10:16:38 +0900

    📝 Props state 실습
    props state 실습

--- my-react-app/src/Mycomponent/11월 11일/App.jsx
+++ my-react-app/src/Mycomponent/11월 11일/App.jsx
@@ -1,58 +1,86 @@
 import { useState } from "react";
-// import { Component } from "react";
 import "./App.css";
 
-// 함수 컴포넌트
-
-function FunctionApp() {
-  const [counter, setCounter] = useState(0);
+function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 0, content: "123" },
+    { id: 1, content: "코딩 공부하기" },
+    { id: 2, content: "잠 자기" },
+  ]);
 
   return (
     <>
-      <Count counter={counter} />
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter((prev) => prev - 1)}>-</button>
+      <TodoList todoList={todoList} setTodoList={setTodoList} />
+      <hr />
+      <TodoInput todoList={todoList} setTodoList={setTodoList} />
     </>
   );
 }
 
-function Count({ counter }) {
-  return <div>counter : {counter}</div>;
+function TodoInput({ todoList, setTodoList }) {
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <>
+      <input
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          const newTodo = { id: Number(new Date()), content: inputValue };
+          const newTodoList = [...todoList, newTodo];
+          setTodoList(newTodoList);
+          setInputValue("");
+        }}
+      >
+        추가하기
+      </button>
+    </>
+  );
 }
 
-// 클래스 컴포넌트
+function TodoList({ todoList, setTodoList }) {
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
+      ))}
+    </ul>
+  );
+}
 
-// class ClassApp extends Component {
-//   state = { counter: 1 };
+function Todo({ todo, setTodoList }) {
+  const [inputValue, setInputValue] = useState("");
+  return (
+    <li>
+      {todo.content}
+      <input
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          setTodoList((prev) =>
+            prev.map((el) =>
+              el.id === todo.id ? { ...el, content: inputValue } : el
+            )
+          );
+        }}
+      >
+        수정
+      </button>
+      <button
+        onClick={() => {
+          setTodoList((prev) => {
+            return prev.filter((el) => el.id !== todo.id);
+          });
+        }}
+      >
+        삭제
+      </button>
+    </li>
+  );
+}
 
-//   render() {
-//     return (
-//       <>
-//         <Count counter={this.state.counter} />
-//         <button
-//           onClick={() => this.setState({ counter: this.state.counter + 1 })}
-//         >
-//           +
-//         </button>
-//         <button
-//           onClick={() => this.setState({ counter: this.state.counter - 1 })}
-//         >
-//           -
-//         </button>
-//       </>
-//     );
-//   }
-// }
-
-// class Count extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-// }
-
-// render() {
-//   return <div>counter: {this.props.counter}</div>;
-// }
-
-export default FunctionApp;
-// export default ClassApp;
+export default App;
