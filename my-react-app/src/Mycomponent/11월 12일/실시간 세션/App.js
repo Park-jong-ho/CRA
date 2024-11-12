e4e35d6042a9079442c648587eee9e9d749d76f1
jongho <whjk1104@gmail.com>
2024-11-12 17:56:46 +0900

    ✨ Style
    style

--- my-react-app/src/Mycomponent/11월 12일/실시간 세션/App.js
+++ my-react-app/src/Mycomponent/11월 12일/실시간 세션/App.js
@@ -1,9 +1,29 @@
 import "./App.css"
 
-unction App() {
+
+
+const App = () => {
+    // 상태 관리 및 조회
+    const name = "my name is react"
+
+    // 함수를 만들어 API 통신, 기타 작업을 합니다.
+
+
+    // 렌더링을 합니다.
+    const style={
+        backgroundColor: "red",
+        color: "blue",
+        fontsize: 24,
+        padding: "1rem",
+    }
+
+
+
+
     return (
         <div>
-            <h1>hi</h1>
+            <h1 className="green_text">hi</h1>
+            <h2 style={style}>{name}</h2>
         </div>
     )
 }
