86a085217a946bfadf5c4cdf143de43aeb64d832
jongho <whjk1104@gmail.com>
2024-11-15 11:48:49 +0900

    ✨ React Hook useEffect
    React Hook useEffect 실습

--- my-react-app/src/Mycomponent/11월 15일/App.jsx
+++ my-react-app/src/Mycomponent/11월 15일/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useState } from "react";
+import { Component, useDebugValue, useEffect, useState } from "react";
 import "./App.css"
 
 function App() {
@@ -13,35 +13,51 @@ function App() {
     )
 }
 
-class Counter extends Component {
-    constructor() {
-        super()
-        this.state = { counter: 1 }
-        console.log("construntor");
-    }
+function Counter() {
+   const [counter, setCounter] = useState(1)
+   const [counter2, setCounter2] = useState(100)
+//------------------------------------
+   // 1. 컴포넌트가 최초로 렌더링 될 때만 조작을 하고 싶다.
+   useEffect(() => {
+    console.log("맨 처음 렌더링 될 때");
+   }, [])
 
-    componentDidMount() {
-        console.log("DidMount");
-    }
+   // 2. 컴포넌트가 리렌더링 될 때 조작하고 싶다!
+   useEffect(() => {
+    console.log("리렌더링...");
+   })
 
-    componentDidUpdate() {
-        console.log("DidUpdate");
-    }
+   // 3. 특정 상태값이 변할 때만 조작하고 싶다.
+   useEffect(() => {
+    console.log("counter의 값이 변할 때");
+   }, [counter])
 
-    componentWillUnmount() {
-        console.log("WillUnMount");
-    }
+   useEffect(() => {
+    console.log("counter2의 값이 변할 때");
+   }, [counter2])
 
-    render() {
-        console.log("render");
-        return (
-            <>
-                <div>counter : {this.state.counter}</div>
-                <button onClick={() => this.setState({counter: this.setState.counter + 1
-                })}>+1</button>
-            </>
-        )
+   // 4. 컴포넌트가 최종적으로 언마운트 될 때 조작하고 싶을 때
+   useEffect(() => {
+    return () => {
+        console.log("returned function");
     }
+   }, [])
+//------------------------------------
+   useEffect(() => {
+    console.log("useEffect");
+    return () => {
+        console.log("returned function");
+    }
+   }, [counter2])
+    
+    return (
+        <>
+            <div>counter : {counter}</div>
+            <button onClick={() => setCounter(counter + 1)}>+1</button>
+            <div>counter2 : {counter2}</div>
+            <button onClick={() => setCounter2(counter2 - 1)}>-1</button>
+        </>
+    )
 }
 
 export default App
