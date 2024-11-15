7aae58781fd8267dea0a888cbe8ec29fbb88bb7c
jongho <whjk1104@gmail.com>
2024-11-15 13:35:16 +0900

    ✨ React 생명 주기 함수
    React 생명 주기 함수

--- my-react-app/src/Mycomponent/11월 15일/App.jsx
+++ my-react-app/src/Mycomponent/11월 15일/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useDebugValue, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css"
 
 function App() {
---
+++ my-react-app/src/Mycomponent/11월 15일/함수 필요성 1/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "React";
+import "./App.css"
+
+function App() {
+    const [data. setData] = useState([])
+
+    useEffect(() => {
+    fetch("http://localhost:3000/data")
+        .then((res) => res.json())
+        .then((res) => setData(res));
+    }, [])
+
+    return (
+        <>
+            <div>데이터 목록</div>
+            {data.map((el) => (<div key={el.id}>{el.content}</div>))}
+        </>
+    )
+}
+
+export default App
